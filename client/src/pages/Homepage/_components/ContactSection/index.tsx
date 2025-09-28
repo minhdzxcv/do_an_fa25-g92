35620ff50a3330e17a4dbd21f1e06d21e169ed99
VinhNTHE171667 <136615934+VinhNTHE171667@users.noreply.github.com>
2025-09-28 21:21:13 +0700

    Update Homepage

--- client/src/pages/Homepage/_components/ContactSection/index.tsx
+++ client/src/pages/Homepage/_components/ContactSection/index.tsx
@@ -18,7 +18,7 @@ const ContactSection = () => {
           <div className={cx("info")}>
             <div className={cx("infoItem")}>
               <i className="fas fa-map-marker-alt"></i>
-              <span>123 Đường ABC, Quận 1, TP. Hồ Chí Minh</span>
+              <span>123 Đường ABC, Quận Cầu Giấy , TP. Hà Nội</span>
             </div>
             <div className={cx("infoItem")}>
               <i className="fas fa-phone-alt"></i>
@@ -32,7 +32,7 @@ const ContactSection = () => {
             <iframe
               className={cx("map")}
               title="Google Map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.502123!2d106.700423!3d10.776889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f3c!2sHo%20Chi%20Minh!5e0!3m2!1sen!2s!4v1695988888888"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59584.985396622506!2d105.74947354586162!3d21.03022160422528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab5756f91033%3A0x576917442d674bfd!2zQ-G6p3UgR2nhuqV5LCBIw6AgTuG7mWksIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1759068655523!5m2!1svi!2s"
               allowFullScreen
               loading="lazy"
             ></iframe>
